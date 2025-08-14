e361081dd193bbfc38c7118eee1d99516d5ff2fe
kdunog <carloseduardo.ndj7@gmail.com>
2025-08-14 13:38:14 -0300

    start project CarControl

---
+++ CarControl/CarControl/src/main/java/com/project/CarControl/CarControlApplication.java
@@ -0,0 +1,13 @@
+package com.project.CarControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarControlApplication.class, args);
+	}
+
+}
---
+++ CarControl/CarControl/src/test/java/com/project/CarControl/CarControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CarControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
