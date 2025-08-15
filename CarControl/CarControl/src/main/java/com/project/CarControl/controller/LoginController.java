19fbb504324c202284a98808ee60657053eb0cad
kdunog <carloseduardo.ndj7@gmail.com>
2025-08-14 21:11:07 -0300

    start project login

--- CarControl/CarControl/src/main/java/com/project/CarControl/controller/LoginController.java
+++ CarControl/CarControl/src/main/java/com/project/CarControl/controller/LoginController.java
@@ -3,24 +3,32 @@ package com.project.CarControl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.project.CarControl.model.Carro;
+import com.project.CarControl.model.Cliente;
 import com.project.CarControl.repository.CarroRepository;
 
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping
 public class LoginController {
 
     @Autowired
     private CarroRepository carroRepository;
 
+
+    @GetMapping("/")
+    public String showLoginForm(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "login"; 
+    }
+
     @PostMapping("/login")
-    public String login(Carro carro, Model model) {
+    public String login(Cliente cliente, Model model) {
         
-        return "redirect:/carros"; // Redirect to the carros page after login
+        return "redirect:/dashboard"; // Redirect to the carros page after login
     }
 
 }
